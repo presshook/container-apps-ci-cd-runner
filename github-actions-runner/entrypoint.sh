b71a62eb53cd58159061f4045a80f47de4a49862
Dustin Blanchard <dustinblanchard@users.noreply.github.com>
2024-04-12 13:07:38 -0500

    Update entrypoint.sh

--- github-actions-runner/entrypoint.sh
+++ github-actions-runner/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -l
+echo "Repository URL: $REPO_URL"
+echo "Registration Token: $REGISTRATION_TOKEN"
 
 ./config.sh --url $REPO_URL --token $REGISTRATION_TOKEN --unattended --ephemeral && ./run.sh
